327760de165f50f5ddcb243a36e28d603d654ead
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2016-03-01 20:58:27 +0100

    Rewritten plugin to work with config object. Added tests.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-param-reassign */
-/* eslint-disable no-console */
 
-import 'regenerator/runtime'
+import 'babel-polyfill'
 import 'isomorphic-fetch'
 import util from 'util'
 import chalk from 'chalk'
 import debug from 'debug'
+import { merge, template, isArray, isString } from 'lodash'
 
 const DEBUG_KEY = 'metalsmith:remote-json-to-files' // See https://github.com/mahnunchik/metalsmith-debug
 const log = debug(DEBUG_KEY)
@@ -16,24 +16,65 @@ function inspect(obj) {
     return `\n${ util.inspect(obj, { colors: debug.useColors() }) }`
 }
 
-export default (opts = {}, callback) => {
-    const { url, fetchOpts } = opts
+const defaultFetchOptions = {
+    headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+    }
+}
+
+const defaultTransformOptions = {}
+
+function transform(json, opts) {
+    if (isArray(json)) {
+        return json.reduce((prev, item) => {
+            const compiled = Object.keys(opts).reduce((obj, key) => {
+                const val = template(opts[key])(item)
+                return {
+                    ...obj,
+                    [key]: key === 'contents' ? new Buffer(val) : val
+                }
+            }, {})
+            return {
+                ...prev,
+                [compiled.filename]: { ...compiled }
+            }
+        }, {})
+    }
+    return {}
+}
+
+export default (fetchOptions = {}, transformOpts = {}) => {
+    const { url, ...rest } = fetchOptions
+    const fetchOpts = merge(defaultFetchOptions, rest)
+    const transformOptions = merge(defaultTransformOptions, transformOpts)
 
     return async (files, metalsmith, done) => {
+
+        // Check required options and parameters
+        if (typeof url === 'undefined') {
+            return done(new Error(`${nok} 'url' parameter must be specified`))
+        }
+        if (!isString(transformOptions.filename)) {
+            return done(new Error(`${nok} 'filename' option is required and must be a string`))
+        }
+        if (!isString(transformOptions.contents)) {
+            return done(new Error(`${nok} 'contents' option is required and must be a string`))
+        }
+
+        // Request JSON
         const json = await fetch(url, fetchOpts)
             .then(response => response.json())
             .catch(err => done(err))
 
         log(`${ok} Fetched from ${url}: `, inspect(json))
 
-        if (typeof callback === 'function') {
-            const newFiles = callback(json, files, metalsmith)
-            files = Object.assign(files, newFiles)
-            log(`${ok} Transformed JSON to files: `, inspect(newFiles))
-            done()
-        } else {
-            log(`${nok} Could not find callback function`)
-            done()
-        }
+        // Transfor the result to files
+        const newFiles = transform(json, transformOptions)
+
+        // Add files to pipeline
+        files = Object.assign(files, newFiles)
+        log(`${ok} Transformed JSON to files: `, inspect(newFiles))
+        done()
     }
 }
--- test/index.js
+++ test/index.js
@@ -2,82 +2,346 @@ import 'isomorphic-fetch'
 import path from 'path'
 import expect from 'expect'
 import equal from 'assert-dir-equal'
-import Metalsmith from 'metalsmith'
 import fetchMock from 'fetch-mock'
-//import fetch from 'node-fetch'
-//import mockery from 'mockery'
+import Metalsmith from 'metalsmith'
 import plugin from '../src'
 
-//fetchMock.useNonGlobalFetch(fetch)
-
-describe('Plugin basics', () => {
-
-    const path = 'test/fixtures/basic'
-
-    beforeEach(() => {
-        fetchMock.mock('http://gihub.com', [{
-            name: '1',
-            body: 'test1\n'
-        }, {
-            name: '2',
-            body: 'test2\n'
-        }])
-    })
+describe('metalsmith-remote-json-to-files', () => {
 
     afterEach(() => {
         fetchMock.restore()
     })
 
-    it('should not make a request without params', (done) => {
-        new Metalsmith(path)
-            .use(plugin())
-            .build(err => {
-                expect(err).toBeAn('object') // TODO: Investigate why error isn't an Error
-                expect(fetchMock.calls().matched.length).toBe(0)
-                expect(fetchMock.calls().unmatched.length).toBe(0)
-                done()
-            });
-    })
+    describe('Plugin basics', () => {
+        beforeEach(() => {
+            fetchMock.mock('http://myurl.test', 200)
+        })
 
-    it('should make a request to a given URL', (done) => {
-        new Metalsmith(path)
-            .use(plugin({
-                url: 'http://gihub.com'
-            }))
-            .build(err => {
-                console.log(err)
-                if (err) {
-                    return done(err)
-                }
+        it(`should throw before requesting data if no url was specified`, (done) => {
+            new Metalsmith('.')
+                .use(plugin())
+                .build(err => {
+                    expect(err.toString()).toInclude(`'url' parameter must be specified`)
+                    expect(fetchMock.calls().matched.length).toBe(0)
+                    expect(fetchMock.calls().unmatched.length).toBe(0)
+                    done()
+                })
+        })
 
-                expect(fetchMock.calls().matched.length).toBe(1)
-                expect(fetchMock.calls().unmatched.length).toBe(0)
-                done()
-            });
-    })
+        it(`should throw if filename option is not defined`, (done) => {
+            new Metalsmith('.')
+                .use(plugin({
+                    url: 'http://myurl.test'
+                }))
+                .build(err => {
+                    expect(err.toString())
+                        .toInclude(`'filename' option is required and must be a string`)
+                    done()
+                })
+        })
 
-    it('should call a callback with JSON as argument', (done) => {
-        new Metalsmith(path)
-            .use(plugin({
-                url: 'http://gihub.com'
-            }, (json) => {
-                return json.reduce((prev, curr) => {
-                    const key = curr.name + '.html'
-                    const transformed = {}
-                    transformed[key] = Object.assign({}, {
-                        title: curr.name,
-                        contents: new Buffer(curr.body)
+        it(`should throw if filename option is not a string`, (done) => {
+            new Metalsmith('.')
+                .use(plugin({
+                    url: 'http://myurl.test'
+                }, {
+                    filename: true
+                }))
+                .build(err => {
+                    expect(err.toString())
+                        .toInclude(`'filename' option is required and must be a string`)
+                    done()
+                })
+        })
+
+        it(`should throw if contents option is not defined`, (done) => {
+            new Metalsmith('.')
+                .use(plugin({
+                    url: 'http://myurl.test'
+                }, {
+                    filename: ''
+                }))
+                .build(err => {
+                    expect(err.toString())
+                        .toInclude(`'contents' option is required and must be a string`)
+                    done()
+                })
+        })
+
+        it(`should throw if contents option is not a string`, (done) => {
+            new Metalsmith('.')
+                .use(plugin({
+                    url: 'http://myurl.test'
+                }, {
+                    filename: '',
+                    contents: 1
+                }))
+                .build(err => {
+                    expect(err.toString())
+                        .toInclude(`'contents' option is required and must be a string`)
+                    done()
+                })
+        })
+
+        it('should make a request to a given URL', (done) => {
+            new Metalsmith('.')
+                .use(plugin({
+                    url: 'http://myurl.test'
+                }, {
+                    filename: '',
+                    contents: ''
+                }))
+                .build(() => {
+                    expect(fetchMock.calls().matched.length).toBe(1)
+                    expect(fetchMock.calls().unmatched.length).toBe(0)
+                    done()
+                })
+        })
+
+        it('should fetch with default options', (done) => {
+            new Metalsmith('.')
+                .use(plugin({
+                    url: 'http://myurl.test'
+                }, {
+                    filename: '',
+                    contents: ''
+                }))
+                .build(() => {
+                    expect(fetchMock.lastOptions()).toEqual({
+                        headers: {
+                            Accept: 'application/json',
+                            'Content-Type': 'application/json'
+                        }
                     })
-                    return Object.assign(prev, transformed)
-                }, {})
-            }))
-            .build(err => {
-                if (err) {
-                    return done(err)
-                }
+                    done()
+                })
+        })
 
-                equal(path + '/expected', path + '/build')
-                done()
-            });
+        it('should merge options', (done) => {
+            new Metalsmith('.')
+                .use(plugin({
+                    url: 'http://myurl.test',
+                    headers: {
+                        'x-test': 'foo'
+                    },
+                    test: 1
+                }, {
+                    filename: '',
+                    contents: ''
+                }))
+                .build(() => {
+                    expect(fetchMock.lastOptions()).toEqual({
+                        headers: {
+                            Accept: 'application/json',
+                            'Content-Type': 'application/json',
+                            'x-test': 'foo'
+                        },
+                        test: 1
+                    })
+                    done()
+                })
+        })
+    })
+
+    describe('Adding files to the pipeline', () => {
+
+        const fixtureData = [{
+            name: '1',
+            body: 'test1'
+        }, {
+            name: '2',
+            body: 'test2'
+        }]
+
+        it('should interpolate filename and body and add files to a pipeline', (done) => {
+            const fixturesPath = 'test/fixtures/basic'
+            fetchMock.mock('http://myurl.test', fixtureData)
+
+            new Metalsmith(fixturesPath)
+                .use(plugin({
+                    url: 'http://myurl.test'
+                }, {
+                    filename: '${name}.html',
+                    contents: 'Wrapped ${body} content\n'
+                }))
+                .build(err => {
+                    expect(err).toBe(null)
+                    equal(path.join(fixturesPath, 'expected'), path.join(fixturesPath, 'build'))
+                    done()
+                })
+
+        })
+
+        it('should interpolate filenames but use ' +
+            'static for body before adding to a pipeline', (done) => {
+            const fixturesPath = 'test/fixtures/filename-interpolate'
+            fetchMock.mock('http://myurl.test', fixtureData)
+
+            new Metalsmith(fixturesPath)
+                .use(plugin({
+                    url: 'http://myurl.test'
+                }, {
+                    filename: 'interpolated-${name}.md',
+                    contents: 'static\n'
+                }))
+                .build(err => {
+                    expect(err).toBe(null)
+                    equal(path.join(fixturesPath, 'expected'), path.join(fixturesPath, 'build'))
+                    done()
+                })
+
+        })
+
+        it('should not change the pipeline for empty JSON reposponse', (done) => {
+            const fixturesPath = 'test/fixtures/no-run'
+            fetchMock.mock('http://myurl.test', [])
+
+            new Metalsmith(fixturesPath)
+                .use(plugin({
+                    url: 'http://myurl.test'
+                }, {
+                    filename: '${name}.md',
+                    contents: 'static\n'
+                }))
+                .build(err => {
+                    expect(err).toBe(null)
+                    equal(path.join(fixturesPath, 'expected'), path.join(fixturesPath, 'build'))
+                    done()
+                })
+        })
+
+        it('should not change the pipeline for empty JSON reposponse', (done) => {
+            const fixturesPath = 'test/fixtures/no-run'
+            fetchMock.mock('http://myurl.test', {})
+
+            new Metalsmith(fixturesPath)
+                .use(plugin({
+                    url: 'http://myurl.test'
+                }, {
+                    filename: '${name}.md',
+                    contents: 'static\n'
+                }))
+                .build(err => {
+                    expect(err).toBe(null)
+                    equal(path.join(fixturesPath, 'expected'), path.join(fixturesPath, 'build'))
+                    done()
+                })
+        })
+
+        it('should stop build with error for errored reposponses', (done) => {
+            const fixturesPath = 'test/fixtures/no-run'
+            fetchMock.mock('http://myurl.test', 404)
+
+            new Metalsmith(fixturesPath)
+                .use(plugin({
+                    url: 'http://myurl.test'
+                }, {
+                    filename: '${name}.md',
+                    contents: 'static\n'
+                }))
+                .build(err => {
+                    expect(err).toNotBe(null)
+                    done()
+                })
+        })
+
+        it('should overwrite existing files in the pipeline', (done) => {
+            const fixturesPath = 'test/fixtures/overwrite'
+            fetchMock.mock('http://myurl.test', fixtureData)
+
+            new Metalsmith(fixturesPath)
+                .use(plugin({
+                    url: 'http://myurl.test'
+                }, {
+                    filename: '${name}.html',
+                    contents: 'overwritten\n'
+                }))
+                .build(err => {
+                    expect(err).toBe(null)
+                    equal(path.join(fixturesPath, 'expected'), path.join(fixturesPath, 'build'))
+                    done()
+                })
+
+        })
+
+        it('should inject all properties to files', (done) => {
+            const fixturesPath = 'test/fixtures/basic'
+            fetchMock.mock('http://myurl.test', fixtureData)
+
+            new Metalsmith(fixturesPath)
+                .use(plugin({
+                    url: 'http://myurl.test'
+                }, {
+                    filename: '${name}.html',
+                    title: 'Title ${name}',
+                    foo: 'bar',
+                    contents: 'Wrapped ${body} content\n'
+                }))
+                .use((files) => {
+                    expect(Object.keys(files).length).toBe(3)
+                    expect(files['1.html'].title).toEqual('Title 1')
+                    expect(files['2.html'].title).toEqual('Title 2')
+                    expect(files['1.html'].foo).toEqual('bar')
+                    expect(files['2.html'].foo).toEqual('bar')
+                })
+                .build(err => {
+                    expect(err).toBe(null)
+                    equal(path.join(fixturesPath, 'expected'), path.join(fixturesPath, 'build'))
+                    done()
+                })
+
+        })
+
+        it.skip('should iterate over deeply nested props of JSON', (done) => {
+            const fixturesPath = 'test/fixtures/no-array'
+            fetchMock.mock('http://myurl.test', {
+                foo: {
+                    bar: [{
+                        name: '1',
+                        body: 'test1'
+                    }, {
+                        name: '2',
+                        body: 'test2'
+                    }]
+                }
+            })
+
+            new Metalsmith(fixturesPath)
+                .use(plugin({
+                    url: 'http://myurl.test'
+                }, {
+                    items: '${foo.bar}',
+                    filename: '${foo}.md',
+                    contents: '${bar}\n'
+                }))
+                .build(err => {
+                    expect(err).toBe(null)
+                    equal(path.join(fixturesPath, 'expected'), path.join(fixturesPath, 'build'))
+                    done()
+                })
+
+        })
+
+        it.skip('should work with a single item object returned in JSON', (done) => {
+            const fixturesPath = 'test/fixtures/no-array'
+            fetchMock.mock('http://myurl.test', {
+                foo: 'single',
+                bar: 'test content'
+            })
+
+            new Metalsmith(fixturesPath)
+                .use(plugin({
+                    url: 'http://myurl.test'
+                }, {
+                    filename: '${foo}.md',
+                    contents: '${bar}\n'
+                }))
+                .build(err => {
+                    expect(err).toBe(null)
+                    equal(path.join(fixturesPath, 'expected'), path.join(fixturesPath, 'build'))
+                    done()
+                })
+
+        })
     })
 })
